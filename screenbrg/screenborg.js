e2224f49fb6de2cc3119b427609b1bbd15fa1d5c
lindosekai <lindosekai@rockbell.(none)>
2013-02-05 16:52:56 -0600

    Ahora podemos crear simples ventanas

--- screenbrg/screenborg.js
+++ screenbrg/screenborg.js
@@ -7,6 +7,31 @@ function screenBorg(){
         console.log("starting...");
         taskBar();
    }
+   this.Window = function(title){
+       	windo = document.createElement("div");
+        windo.style.backgroundColor = "#000";
+        windo.style.color = "#03f";
+       windo.style.width = 100;
+       windo.style.height = 100;
+
+       windo.style.position = "absolute";
+       windo.style.top = 10;
+       windo.style.right = 10;
+       windo.style.border = "2px black solid";
+       windo.style.backgroundColor = "#05f";
+       titlebar = document.createElement("div");
+       titlebar.style.color = "white";
+       titlebar.innerHTML = title;
+       titlebar.style.backgroundColor = "#000";
+
+       windo.appendChild(titlebar);
+        var body = document.getElementsByTagName("body");
+        body[0].appendChild(windo);
+        console.log("starting... window : " + title);
+}
+
+
+
    var taskBar = function(){
         taskbr = document.createElement("div");
         taskbr.innerHTML = "the task";
