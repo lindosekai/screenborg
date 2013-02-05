a56b8c3388d7bcacee628b65968731270415585d
lindosekai <lindosekai@rockbell.(none)>
2013-02-05 13:56:59 -0600

    Listo la primera parte del sistema grafico

--- screenbrg/screenborg.js
+++ screenbrg/screenborg.js
@@ -10,8 +10,17 @@ function screenBorg(){
    var taskBar = function(){
         taskbr = document.createElement("div");
         taskbr.innerHTML = "the task";
+        taskbr.style.backgroundColor = "#000";
+        taskbr.style.color = "#03f";
+        taskbr.style.position = "absolute";
+        taskbr.style.bottom = 0;
+        taskbr.style.left = 0;
+        taskbr.style.width = "100%";
         var body = document.getElementsByTagName("body");
         body[0].appendChild(taskbr);
+        body[0].style.padding = 0;
+        body[0].style.margin = 0;
+        body[0].style.backgroundColor = "#aaa";
         console.log("starting... tb");
    }
 
